7368f8ebe7450b3d831228b457fc1a59aa51612d
RYAN JAMES <realcoder2023@gmail.com>
2025-11-27 23:13:52 -0500

    added background styles and headrer styles to main UI page

--- public/client.ts
+++ public/client.ts
@@ -1,6 +1,6 @@
 const clickableButton = document.getElementById("button");
 
-clickableButton.addEventListener("click", getDataBackClient)
+clickableButton?.addEventListener("click", getDataBackClient);
 
 function getDataBackClient() {
   alert("WEB Dev is AMAZING...");
